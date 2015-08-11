2b23cf2022d03ba0be2b378f5ae2f311823925ec
lykovrs <lykovrs@gmail.com>
2015-08-11 17:43:24 +0300

    Create forecast.json

--- js/services/forecast.js
+++ js/services/forecast.js
@@ -1,5 +1,5 @@
 app.factory('forecast', ['$http', function($http) {
-    return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/forecast-api/forecast.json')
+    return $http.get('forecast.json')
         .success(function(data) {
             return data;
         })
